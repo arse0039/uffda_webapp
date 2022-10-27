87aa62899e55f4ae1214347755610a94651b87a1
arse0039 <arse0039@umn.edu>
2022-10-27 16:19:00 -0700

    Added styling and script for display of forms

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const activityInsert = () => {
+    document.querySelector
+
+}
+
+const activityUpdate = () => {
+    document.getElementById("activity-update").style.visibility ='visible';
+    document.getElementByClass("main").style.filter = 'blur(10px)';
+}
+
+const activityDel = () => {
+
+
+}
+
