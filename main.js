001571536ac4408ff8e590526d07fe4865c5ddc8
arse0039 <arse0039@umn.edu>
2022-10-27 19:08:31 -0700

    Ready to deploy!

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-
-const activityInsert = () => {
-    document.querySelector
-
+function showform(formtype){
+    if (formtype == "edit") {
+        document.getElementById("activity-insert").style.visibility="hidden"
+        document.getElementById("activity-update").style.visibility="visible"
+        document.getElementById("activity-delete").style.visibility="hidden"
+        document.getElementById("table").style.filter="blur(3px)"
+        document.getElementById("header").style.filter="blur(3px)"    
+    }else if (formtype == "delete") {
+        document.getElementById("activity-insert").style.visibility="hidden"
+        document.getElementById("activity-update").style.visibility="hidden"
+        document.getElementById("activity-delete").style.visibility="visible"
+        document.getElementById("table").style.filter="blur(3px)"
+        document.getElementById("header").style.filter="blur(3px)"                 
+    } else if (formtype == "add") {
+        document.getElementById("activity-insert").style.visibility="visible"
+        document.getElementById("activity-update").style.visibility="hidden"
+        document.getElementById("activity-delete").style.visibility="hidden"
+        document.getElementById("table").style.filter="blur(3px)"
+        document.getElementById("header").style.filter="blur(3px)" 
+    } else {
+        document.getElementById("activity-insert").style.visibility="hidden"
+        document.getElementById("activity-update").style.visibility="hidden"
+        document.getElementById("activity-delete").style.visibility="hidden"
+        document.getElementById("table").style.filter="blur(0px)"
+        document.getElementById("header").style.filter="blur(0px)" 
+    }
 }
-
-const activityUpdate = () => {
-    document.getElementById("activity-update").style.visibility ='visible';
-    document.getElementByClass("main").style.filter = 'blur(10px)';
-}
-
-const activityDel = () => {
-
-
-}
+function editActivity() {showform("edit");}
+function deleteActivity() {showform("delete");}
+function addActivity() {showform("insert");}
+function closeForm() {showform("close");}
 
