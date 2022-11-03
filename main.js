b928c24d3308e2cecc345cc3b0c7bdfc800a2ce5
arse0039 <arse0039@umn.edu>
2022-11-03 14:21:40 -0700

    Participants page styled

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
 function showform(formtype){
     if (formtype == "edit") {
-        document.getElementById("activity-insert").style.visibility="hidden"
-        document.getElementById("activity-update").style.visibility="visible"
-        document.getElementById("activity-delete").style.visibility="hidden"
-        document.getElementById("table").style.filter="blur(3px)"
+        document.getElementById("insert-form").style.visibility="hidden"
+        document.getElementById("update-form").style.visibility="visible"
+        document.getElementById("delete-form").style.visibility="hidden"
+        document.getElementById("table-div").style.filter="blur(3px)"
         document.getElementById("header").style.filter="blur(3px)"    
         document.getElementById("search-div").style.filter="blur(3px)"
     }else if (formtype == "delete") {
-        document.getElementById("activity-insert").style.visibility="hidden"
-        document.getElementById("activity-update").style.visibility="hidden"
-        document.getElementById("activity-delete").style.visibility="visible"
-        document.getElementById("table").style.filter="blur(3px)"
-        document.getElementById("header").style.filter="blur(3px)"                 
+        document.getElementById("insert-form").style.visibility="hidden"
+        document.getElementById("update-form").style.visibility="hidden"
+        document.getElementById("delete-form").style.visibility="visible"
+        document.getElementById("table-div").style.filter="blur(3px)"
+        document.getElementById("header").style.filter="blur(3px)"
+        document.getElementById("search-div").style.filter="blur(3px)"               
     } else if (formtype == "insert") {
-        document.getElementById("activity-insert").style.visibility="visible"
-        document.getElementById("activity-update").style.visibility="hidden"
-        document.getElementById("activity-delete").style.visibility="hidden"
-        document.getElementById("table").style.filter="blur(3px)"
+        document.getElementById("insert-form").style.visibility="visible"
+        document.getElementById("update-form").style.visibility="hidden"
+        document.getElementById("delete-form").style.visibility="hidden"
+        document.getElementById("table-div").style.filter="blur(3px)"
         document.getElementById("header").style.filter="blur(3px)" 
+        document.getElementById("search-div").style.filter="blur(3px)"
     } else {
-        document.getElementById("activity-insert").style.visibility="hidden"
-        document.getElementById("activity-update").style.visibility="hidden"
-        document.getElementById("activity-delete").style.visibility="hidden"
-        document.getElementById("table").style.filter="blur(0px)"
+        document.getElementById("insert-form").style.visibility="hidden"
+        document.getElementById("update-form").style.visibility="hidden"
+        document.getElementById("delete-form").style.visibility="hidden"
+        document.getElementById("table-div").style.filter="blur(0px)"
         document.getElementById("header").style.filter="blur(0px)" 
         document.getElementById("search-div").style.filter="blur(0px)"
     }
 }
-function editActivity() {showform("edit");}
-function deleteActivity() {showform("delete");}
-function addActivity() {showform("insert");}
+function edit() {showform("edit");}
+function del() {showform("delete");}
+function add() {showform("insert");}
 function closeForm() {showform("close");}
 
